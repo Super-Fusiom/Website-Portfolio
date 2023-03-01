e522d3727c402b112e85bea51d970cb48550b140
Paul <paul21479@gmail.com>
2023-03-01 21:55:42 +1300

    added spicy css

--- src/components/homeSplash/homeSplash.tsx
+++ src/components/homeSplash/homeSplash.tsx
@@ -2,9 +2,9 @@ import "./homeSplash.css";
 
 export default function homeSplash() {
   return (
-    <>
+    <header className="title">
       <h1>Hey there, I'm Paul Singh</h1>
       <h1>Or should I say Super_Fusiom</h1>
-    </>
+    </header>
   );
 }
