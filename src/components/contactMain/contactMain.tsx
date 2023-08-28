8ef2ad93a650e5fe119126737c6631fee79a5676
Paul <paul21479@gmail.com>
2023-08-29 11:31:22 +1200

    Added Github link, changed default tab title

--- src/components/contactMain/contactMain.tsx
+++ src/components/contactMain/contactMain.tsx
@@ -1,17 +1,26 @@
 import "./contactMain.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const linkdenLogo = <FontAwesomeIcon icon={faLinkedin} />;
+const githubLogo = <FontAwesomeIcon icon={faGithub} />;
 
 const contactMain = () => {
   return (
     <>
       <h1>If you wish to contact me, please contact me at</h1>
-      <div className="social"></div>
-      <h2>
-        <a href="https://www.linkedin.com/in/super-fusiom">{linkdenLogo}</a>
-      </h2>
+      <div className="social">
+        <ul>
+          <li>
+            <h3>Linkden</h3>
+            <a href="https://www.linkedin.com/in/super-fusiom">{linkdenLogo}</a>
+          </li>
+          <li>
+            <h3>Github</h3>
+            <a href="https://github.com/Super-Fusiom">{githubLogo}</a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
