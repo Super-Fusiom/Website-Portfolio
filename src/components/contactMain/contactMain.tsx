a3d483d45e8043fcdcc6fc4d9a021daaabc1f0d2
Paul <paul21479@gmail.com>
2023-09-05 21:43:31 +1200

    Added mail

--- src/components/contactMain/contactMain.tsx
+++ src/components/contactMain/contactMain.tsx
@@ -1,9 +1,11 @@
 import "./contactMain.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const linkdenLogo = <FontAwesomeIcon icon={faLinkedin} />;
 const githubLogo = <FontAwesomeIcon icon={faGithub} />;
+const mailLogo = <FontAwesomeIcon icon={faEnvelope} />;
 
 const contactMain = () => {
   return (
@@ -18,6 +20,10 @@ const contactMain = () => {
           <li>
             <h3>Github</h3>
             <a href="https://github.com/Super-Fusiom">{githubLogo}</a>
+          </li>
+          <li>
+            <h3>Mail</h3>
+            <a href="mailto:paul21479@gmail.com">{mailLogo}</a>
           </li>
         </ul>
       </div>
