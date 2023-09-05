f51d840305fb4c6127d6a417245204d600968458
Paul <paul21479@gmail.com>
2023-09-05 21:45:13 +1200

    Casual time

--- src/components/homeSplash/homeSplash.tsx
+++ src/components/homeSplash/homeSplash.tsx
@@ -3,8 +3,9 @@ import "./homeSplash.css";
 export default function homeSplash() {
   return (
     <header className="title">
-      <h1>Hey there, I'm Paul Singh</h1>
+      <h1>Hey there, I'm Paul</h1>
       <h1>Or should I say Super_Fusiom</h1>
+      <h1>You know what? Call me whatever you like i guess</h1>
     </header>
   );
 }
